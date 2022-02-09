afdf9aa47c6cd151d36c4c4b5c996ffdb1f88525
CJSmith325 <90848885+CJSmith325@users.noreply.github.com>
2022-02-09 12:47:36 -0500

    Audio/particle additions

---
+++ Assets/Scripts/Particle.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    // simulation space setting: local -> world allows particles to travel with bullet prefab
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -4,10 +4,35 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
+    public float bulletLife;
     public Transform firePoint;
     public GameObject bulletPrefab;
+    public AudioClip audioClip;
+    private AudioSource audioSource; //The thing to play the audio
 
     public float bulletForce = 20f;
+
+    void PlayShootingSound()
+    {
+        
+        //If you need to change the volume
+        //audioSource.volume = volume;
+        //Play the sound once
+        audioSource.PlayOneShot(audioClip);
+
+
+       
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioClip = GetComponent<AudioClip>();
+        if (audioSource == null)
+        {
+            Debug.LogError("No AudioSource found");
+        }
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,6 +40,7 @@ public class Shooting : MonoBehaviour
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
+            PlayShootingSound();
         }
     }
 
@@ -23,5 +49,6 @@ public class Shooting : MonoBehaviour
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+        Destroy(bullet, bulletLife);
     }
 }
